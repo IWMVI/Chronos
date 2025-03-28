bd3c87f6693a056a57414cc444fff2123309bedd
Wallace Martins <wmvwallace@gmail.com>
2025-03-28 00:05:06 -0300

    feat: substituir componentes de layout por novos contêineres e cabeçalhos estilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
