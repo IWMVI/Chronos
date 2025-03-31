de8286550b22a5ce4ed531a31b8234b94e96b59a
Wallace Martins <wmvwallace@gmail.com>
2025-03-31 17:34:06 -0300

    refactor(App): remover contagem e botão de incremento, ajustando rótulo do DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
+import { Menu } from "./components/Menu";
+import { Logo } from "./components/Logo";
+import { Footer } from "./components/Footer";
+import { Cycles } from "./components/Cycles";
 import { PlayCircleIcon } from "lucide-react";
-import { useState } from "react";
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
 import { DefaultInput } from "./components/DefaultInput";
-import { Footer } from "./components/Footer";
-import { Heading } from "./components/Heading";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
+import { DefaultButton } from "./components/DefaultButton";
 
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
-  // const [numero, setNumero] = useState(() => {
-  //   console.log("Lazy initialization");
-  //   return 0;
-  // });
-
-  const [numero, setNumero] = useState(0);
-
-  function handleClick() {
-    setNumero((prevState) => prevState + 1);
-  }
-
   return (
     <>
-      <Heading>
-        <span>{numero}</span>
-        <button onClick={handleClick}>Aumenta</button>
-      </Heading>
-
       <Container>
         <Logo />
       </Container>
@@ -45,7 +27,7 @@ export function App() {
         <form className="form" action="">
           <div className="formRow">
             <DefaultInput
-              labelText={numero.toString()}
+              labelText="Task"
               id="myInput"
               type="text"
               placeholder="Informe uma Task"
