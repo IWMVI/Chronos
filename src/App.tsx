6ab70c2cd1b81552f60708538e9cdf585773085b
Wallace Martins <wmvwallace@gmail.com>
2025-03-31 17:13:12 -0300

    feat(Exemplo): adicionar contagem e botão para incrementar valor no componente App utilizando useState().

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import { PlayCircleIcon } from "lucide-react";
+import { useState } from "react";
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
 import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { DefaultInput } from "./components/DefaultInput";
+import { Footer } from "./components/Footer";
+import { Heading } from "./components/Heading";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
-import { Footer } from "./components/Footer";
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
+  // const [numero, setNumero] = useState(() => {
+  //   console.log("Lazy initialization");
+  //   return 0;
+  // });
+
+  const [numero, setNumero] = useState(0);
+
+  function handleClick() {
+    setNumero((prevState) => prevState + 1);
+  }
+
   return (
     <>
+      <Heading>
+        <span>{numero}</span>
+        <button onClick={handleClick}>Aumenta</button>
+      </Heading>
+
       <Container>
         <Logo />
       </Container>
@@ -27,7 +45,7 @@ export function App() {
         <form className="form" action="">
           <div className="formRow">
             <DefaultInput
-              labelText="Task"
+              labelText={numero.toString()}
               id="myInput"
               type="text"
               placeholder="Informe uma Task"
