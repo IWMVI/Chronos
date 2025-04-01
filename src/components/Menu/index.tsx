9f659d8cc00ead7dd64e7ad206531923c79ba4c5
Wallace Martins <wmvwallace@gmail.com>
2025-04-01 15:15:39 -0300

    feat(Menu): adicionar suporte a troca de tema e atualiza acessibilidade dos links

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,61 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
+type AvailableThemes = "dark" | "light";
+
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function handleThemeChange() {
+    event?.preventDefault();
+    console.log("Clicado", Date.now());
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+    // document.documentElement.setAttribute("data-theme", theme);
+  }
+
+  useEffect(() => {
+    console.log(Date.now());
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a href="#" className={styles.menuLink} title="Home">
+      <h1>{theme}</h1>
+      <a
+        href="#"
+        className={styles.menuLink}
+        title="Ir para home"
+        aria-label="Ir para a home"
+      >
         <HouseIcon />
       </a>
-      <a href="#" className={styles.menuLink} title="History">
+      <a
+        href="#"
+        className={styles.menuLink}
+        title="Ver histórico"
+        aria-label="Ver histórico"
+      >
         <HistoryIcon />
       </a>
-      <a href="#" className={styles.menuLink} title="Settings">
+      <a
+        href="#"
+        className={styles.menuLink}
+        title="Ir para configurações"
+        aria-label="Ir para configurações"
+      >
         <SettingsIcon />
       </a>
-      <a href="#" className={styles.menuLink} title="Sun">
+      <a
+        href="www.google.com"
+        className={styles.menuLink}
+        title="Alterar tema"
+        aria-label="Alterar tema"
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
